08432dd2465ff63320b2ba780e2023c795d61253
sackboynick <nicola.santolini@live.com>
2021-11-15 15:04:12 +0100

    Initial commit

---
+++ ChatSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChatSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
