2241a3a8dd7e0e7cf4c8b7be0f117705f6dd781f
hennyg2001 <henrikmiki@gmail.com>
2021-11-22 19:11:34 +0100

    Domain Changes 4

--- Domain/User/IUserValidation.cs
+++ Domain/User/IUserValidation.cs
@@ -1,9 +1,7 @@
-using Entities;
-
 namespace Domain.User
 {
     public interface IUserValidation
     {
-        public User ValidateUser(string userName, string password);
+        public Entities.User ValidateUser(string userName, string password);
     }
 }
