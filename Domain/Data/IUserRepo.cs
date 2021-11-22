1531310fe5e9c8414d30d1282300ec87d9db1278
hennyg2001 <henrikmiki@gmail.com>
2021-11-22 18:33:30 +0100

    domain classes

--- Domain/Data/IUserRepo.cs
+++
@@ -1,7 +0,0 @@
-namespace Domain.Data
-{
-    public interface IUserRepo
-    {
-        
-    }
-}
--- Domain/User/IUserValidation.cs
+++
@@ -1,7 +0,0 @@
-namespace Domain.User
-{
-    public interface IUserValidation
-    {
-        
-    }
-}
