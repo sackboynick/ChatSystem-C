a3da3fb8e7fd693edb61b1cce12d9dbe84e80925
Luka Udovicic <lukaudovicic10@gmail.com>
2021-12-03 10:43:16 +0100

    Search functionality but doesnt work properly

--- Blazor/obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
+++ Blazor/obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
@@ -13,70 +13,70 @@ namespace Blazor
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/home/c/Desktop/RiderProjects/ChatSystem/Blazor/_Imports.razor"
+#line 1 "E:\3RD SEMESTER\ChatSystem-C\Blazor\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/home/c/Desktop/RiderProjects/ChatSystem/Blazor/_Imports.razor"
+#line 2 "E:\3RD SEMESTER\ChatSystem-C\Blazor\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/home/c/Desktop/RiderProjects/ChatSystem/Blazor/_Imports.razor"
+#line 3 "E:\3RD SEMESTER\ChatSystem-C\Blazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/home/c/Desktop/RiderProjects/ChatSystem/Blazor/_Imports.razor"
+#line 4 "E:\3RD SEMESTER\ChatSystem-C\Blazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/home/c/Desktop/RiderProjects/ChatSystem/Blazor/_Imports.razor"
+#line 5 "E:\3RD SEMESTER\ChatSystem-C\Blazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/home/c/Desktop/RiderProjects/ChatSystem/Blazor/_Imports.razor"
+#line 6 "E:\3RD SEMESTER\ChatSystem-C\Blazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/home/c/Desktop/RiderProjects/ChatSystem/Blazor/_Imports.razor"
+#line 7 "E:\3RD SEMESTER\ChatSystem-C\Blazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/home/c/Desktop/RiderProjects/ChatSystem/Blazor/_Imports.razor"
+#line 8 "E:\3RD SEMESTER\ChatSystem-C\Blazor\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/home/c/Desktop/RiderProjects/ChatSystem/Blazor/_Imports.razor"
+#line 9 "E:\3RD SEMESTER\ChatSystem-C\Blazor\_Imports.razor"
 using Blazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/home/c/Desktop/RiderProjects/ChatSystem/Blazor/_Imports.razor"
+#line 10 "E:\3RD SEMESTER\ChatSystem-C\Blazor\_Imports.razor"
 using Blazor.Shared;
 
 #line default
