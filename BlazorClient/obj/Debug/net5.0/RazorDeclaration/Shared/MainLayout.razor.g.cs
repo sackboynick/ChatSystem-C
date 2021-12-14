979574f0a852e3e49da39c881c7d67da3ade2a14
hennyg2001 <henrikmiki@gmail.com>
2021-12-14 03:04:55 +0100

    stuff

--- BlazorClient/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ BlazorClient/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,84 +13,84 @@ namespace BlazorClient.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/home/c/Desktop/RiderProjects/ChatSystem/BlazorClient/_Imports.razor"
+#line 1 "/Users/henrikkoster/Documents/CookAway/ChatSystem-C/BlazorClient/_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/home/c/Desktop/RiderProjects/ChatSystem/BlazorClient/_Imports.razor"
+#line 2 "/Users/henrikkoster/Documents/CookAway/ChatSystem-C/BlazorClient/_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/home/c/Desktop/RiderProjects/ChatSystem/BlazorClient/_Imports.razor"
+#line 3 "/Users/henrikkoster/Documents/CookAway/ChatSystem-C/BlazorClient/_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/home/c/Desktop/RiderProjects/ChatSystem/BlazorClient/_Imports.razor"
+#line 4 "/Users/henrikkoster/Documents/CookAway/ChatSystem-C/BlazorClient/_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/home/c/Desktop/RiderProjects/ChatSystem/BlazorClient/_Imports.razor"
+#line 5 "/Users/henrikkoster/Documents/CookAway/ChatSystem-C/BlazorClient/_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/home/c/Desktop/RiderProjects/ChatSystem/BlazorClient/_Imports.razor"
+#line 6 "/Users/henrikkoster/Documents/CookAway/ChatSystem-C/BlazorClient/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/home/c/Desktop/RiderProjects/ChatSystem/BlazorClient/_Imports.razor"
+#line 7 "/Users/henrikkoster/Documents/CookAway/ChatSystem-C/BlazorClient/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/home/c/Desktop/RiderProjects/ChatSystem/BlazorClient/_Imports.razor"
+#line 8 "/Users/henrikkoster/Documents/CookAway/ChatSystem-C/BlazorClient/_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/home/c/Desktop/RiderProjects/ChatSystem/BlazorClient/_Imports.razor"
+#line 9 "/Users/henrikkoster/Documents/CookAway/ChatSystem-C/BlazorClient/_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/home/c/Desktop/RiderProjects/ChatSystem/BlazorClient/_Imports.razor"
+#line 10 "/Users/henrikkoster/Documents/CookAway/ChatSystem-C/BlazorClient/_Imports.razor"
 using BlazorClient;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "/home/c/Desktop/RiderProjects/ChatSystem/BlazorClient/_Imports.razor"
+#line 11 "/Users/henrikkoster/Documents/CookAway/ChatSystem-C/BlazorClient/_Imports.razor"
 using BlazorClient.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/home/c/Desktop/RiderProjects/ChatSystem/BlazorClient/Shared/MainLayout.razor"
+#line 2 "/Users/henrikkoster/Documents/CookAway/ChatSystem-C/BlazorClient/Shared/MainLayout.razor"
 using BlazorClient.LoginComponent;
 
 #line default
