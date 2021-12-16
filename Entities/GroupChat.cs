2fddf3fcec56609271493840d84b369c102b3371
sackboynick <nicola.santolini@live.com>
2021-12-16 16:11:07 +0100

    GroupChat

--- Entities/GroupChat.cs
+++ Entities/GroupChat.cs
@@ -29,7 +29,7 @@ namespace Entities
         {
             GroupName = groupName;
             Participants =new List<Participant> {new(groupCreator,true)};
-            Messages.Add(new Message("Server", Participants.ToList()[0]+" just created the group on date "+ DateTime.Now.ToString("MM/dd/yyyy h:mm tt")));
+            Messages.Add(new Message("Server", Participants.ToList()[0]+" just created the group on date "+ DateTime.Now.ToString("MM/dd/yyyy h:mm:ss")));
             Console.WriteLine(groupCreator);
         }
 
