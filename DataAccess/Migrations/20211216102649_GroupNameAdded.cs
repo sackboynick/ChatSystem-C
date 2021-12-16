8745a424785f34ec5b671b4a85b388f591023482
sackboynick <nicola.santolini@live.com>
2021-12-16 11:28:04 +0100

    DatabaseUpdates

---
+++ DataAccess/Migrations/20211216102649_GroupNameAdded.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class GroupNameAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GroupName",
+                table: "GroupChats",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GroupName",
+                table: "GroupChats");
+        }
+    }
+}
